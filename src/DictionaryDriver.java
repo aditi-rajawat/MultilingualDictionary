537a886db7fa765b3728104c6102249b1ff77f0b
Rajawat <arajawat@ptc.com>
2016-03-02 12:33:00 -0800

    First release of Mapper implementation for testing

--- src/DictionaryDriver.java
+++ src/DictionaryDriver.java
@@ -4,7 +4,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
@@ -22,7 +22,7 @@ public class DictionaryDriver extends Configured implements Tool {
       job.setReducerClass(DictionaryReducer.class);
       job.setOutputKeyClass(Text.class);
       job.setOutputValueClass(Text.class);
-      job.setInputFormatClass(KeyValueTextInputFormat.class);
+      job.setInputFormatClass(TextInputFormat.class);
       FileInputFormat.addInputPath(job, new Path(args[0]));
       FileOutputFormat.setOutputPath(job, new Path(args[1]));
       return job.waitForCompletion(true) ? 0:1;
